515a69a0dfcfc6c5ad770adbb27e0cf2e8c5e9dd
unknown <tcutlip08@gmail.com>
2019-06-27 14:46:24 -0400

    made buttons open close new window to answer questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,16 @@
+$(function () {
+    var questions = {
+        adding: { question: "2+2", answer: "4" },
+        subtraction: { question: "5-2", answer: "3" },
+        multiplication: { question: "4*6", answer: "24" },
+        division: { question: "12/4", answer: "3" },
+        fraction: {question: "10/4", answer: "5/2" },
+        percentages: {question: "20*10%", answer: "2"}
+    };
+    console.log(questions)
+});
 
-
-
-
-
-
-function hideButton(id){
+function disableButton(id) {
     $(id).css("display", "none");
+
 }
