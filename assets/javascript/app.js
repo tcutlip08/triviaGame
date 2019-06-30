bd305b78ee8bcc5d93ce7c5c1dc87830d5c224c7
unknown <tcutlip08@gmail.com>
2019-06-30 16:21:27 -0400

    can pull the id name of each option, still trouble shooting how to push info on displayQuestions html file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,23 +21,30 @@ $(function () {
             300: {}
         }
     };
-
-
+    $(document).on("click", ".question", function () {
+        displayQuestionAnswers($(this).attr("id"), questions);
+    });
 
 });
 
 
 
-$("#add1").on("click", function(){
-    console.log("hello");
-});
 
 function disableButton(id) {
-    setTimeout(function(){
+    setTimeout(function () {
         $(id).css("display", "none");
     }, 500);
 }
 
-function displayQuestionAnswers (id){
-    
+function displayQuestionAnswers(id, questions) {
+
+    if (id[0] === "a") {
+        var i = id.length - 1;
+        if (id[i] === "1") {
+            
+            $(function (){
+                $("#option1").text("Fuck");
+            });
+        }
+    }
 }
