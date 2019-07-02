a0be3361f22fd2d24e323d3c423cb512ec72357a
unknown <tcutlip08@gmail.com>
2019-07-01 20:19:12 -0400

    created the rest of the categories answers/questions, tests the option chosen to see if its correct or not. sets the button green or red if they are right. disables buttons & re enables when you click a new question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,144 @@
 $(function () {
     var questions = {
-        adding: {
-            a100: { question: "2+2", answer: "4" },
-            a200: {},
-            a300: {}
+        addition: {
+            a100: { question: "3+2", answer: "5" },
+            a200: { question: "13+8", answer: "21" },
+            a300: { question: "54+67", answer: "121" }
         },
         subtraction: {
             s100: { question: "5-2", answer: "3" },
-            s200: {},
-            s300: {}
+            s200: { question: "31-12", answer: "19" },
+            s300: { question: "101-33", answer: "68" }
         },
         multiplication: {
-            m100: { question: "4*6", answer: "24" },
-            m200: {},
-            m300: {}
+            m100: { question: "4*3", answer: "12" },
+            m200: { question: "12*7", answer: "84" },
+            m300: { question: "13*14", answer: "182" }
         },
         division: {
             d100: { question: "12/4", answer: "3" },
-            d200: {},
-            d300: {}
+            d200: { question: "104/4", answer: "26" },
+            d300: { question: "608/8", answer: "76" }
         }
     };
     $(document).on("click", ".question", function () {
+        enableOptionButtons();
         displayQuestionCreateAnswers($(this).attr("id"), questions);
     });
 
 });
 
-// Math.floor(Math.random() * Math.floor(max));
 
+function createRandomAnswers(id, answers, valuesIncluded, i, j) {
+    while (typeof answers[j] === "string") {
+        if (id[i] === "1") {
+            var ranNum = Math.floor(Math.random() * Math.floor(15) + 1);
+        } else if (id[i] === "2") {
+            var ranNum = Math.floor(Math.random() * Math.floor(70) + 20);
+        } else {
+            var ranNum = Math.floor(Math.random() * Math.floor(100) + 50);
+        }
+        if (!valuesIncluded.includes(ranNum)) {
+            answers[j] = ranNum;
+        }
+    }
+
+    return answers[j];
+}
+
+function displayRandomAnswers(answers) {
+
+    for (let i = 0; i < answers.length; i++) {
+        var usedOptions = [];
+        var test = 0;
+        while (test < answers.length) {
+            var option = Math.floor(Math.random() * Math.floor(answers.length));
+
+            if (!usedOptions.includes(option)) {
+                test++;
+                $("#option" + test).text(answers[option]);
+                $("#option" + test).attr("value", answers[option]);
+                usedOptions.push(option);
+            }
+        }
+    }
+}
+
+function testIfTheyAreRight(test, answers) {
+    if (test === answers[0]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function answerClicks(answers) {
+    $("#option1").on("click", function () {
+        test = parseInt($("#option1").val());
+        var bool = testIfTheyAreRight(test, answers);
+        if (bool === true) {
+            $(this).attr("class", "btn btn-success");
+            disableOptionButtons();
+        } else {
+            $(this).attr("class", "btn btn-danger");
+            disableOptionButtons();
+        }
+    });
+
+    $("#option2").on("click", function () {
+        test = parseInt($("#option2").val());
+        var bool = testIfTheyAreRight(test, answers);
+        if (bool === true) {
+            $(this).attr("class", "btn btn-success");
+            disableOptionButtons();
+        } else {
+            $(this).attr("class", "btn btn-danger");
+            disableOptionButtons();
+        }
+    });
+
+    $("#option3").on("click", function () {
+        test = parseInt($("#option3").val());
+        var bool = testIfTheyAreRight(test, answers);
+        if (bool === true) {
+            $(this).attr("class", "btn btn-success");
+            disableOptionButtons();
+        } else {
+            $(this).attr("class", "btn btn-danger");
+            disableOptionButtons();
+        }
+    });
+
+    $("#option4").on("click", function () {
+        test = parseInt($("#option4").val());
+        var bool = testIfTheyAreRight(test, answers);
+        if (bool === true) {
+            $(this).attr("class", "btn btn-success");
+            disableOptionButtons();
+        } else {
+            $(this).attr("class", "btn btn-danger");
+            disableOptionButtons();
+        }
+    });
+}
+
+function disableOptionButtons() {
+    $("#option1").prop('disabled', true);
+    $("#option2").prop('disabled', true);
+    $("#option3").prop('disabled', true);
+    $("#option4").prop('disabled', true);
+};
+
+function enableOptionButtons() {
+    $("#option1").prop('disabled', false);
+    $("#option1").attr("class", "btn btn-secondary");
+    $("#option2").prop('disabled', false);
+    $("#option2").attr("class", "btn btn-secondary");
+    $("#option3").prop('disabled', false);
+    $("#option3").attr("class", "btn btn-secondary");
+    $("#option4").prop('disabled', false);
+    $("#option4").attr("class", "btn btn-secondary");
+};
 
 function disableButton(id) {
     $(id).css("display", "none");
@@ -38,69 +148,195 @@ function displayQuestionCreateAnswers(id, questions) {
 
     if (id[0] === "a") {
         var i = id.length - 1;
+        var valuesIncluded = [];
         if (id[i] === "1") {
-            $("#question").text(questions.adding.a100.question);
-            let valuesIncluded = [];
+            $("#question").text(questions.addition.a100.question);
             var answers = ["1", "2", "3", "4"];
 
-            for (let i = 0; i < answers.length; i++) {
-                answers[0] = parseInt(questions.adding.a100.answer);
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.addition.a100.answer);
 
-                while (typeof answers[i] === "string") {
-                    let ranNum = Math.floor(Math.random() * Math.floor(9) + 1);
-                    if (!valuesIncluded.includes(ranNum)) {
-                        answers[i] = ranNum;
-                    }
-                }
-
-                valuesIncluded.push(answers[i]);
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
             }
 
             console.log(answers.join(", "));
-            displayAnswers(answers);
+            displayRandomAnswers(answers);
 
-            optionPicked();
-        }
-    }
-}
+            answerClicks(answers);
+        } else if (id[i] === "2") {
+            $("#question").text(questions.addition.a200.question);
+            var answers = ["1", "2", "3", "4"];
 
-function displayAnswers(answers) {
-    // let i = 0;
-    // while (answers.length !== 0){
-    //     let answersUsed = [];
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.addition.a200.answer);
 
-    // }
-
-    for (let i = 0; i < answers.length; i++) {
-        var usedOptions = [];
-        var test = 0;
-        while (test < answers.length) {
-            var option = Math.floor(Math.random() * Math.floor(answers.length)).toString(8);
-
-            if (!usedOptions.includes(option)) {
-                test++;
-                $("#option" + test).text(answers[option]);
-                usedOptions.push(option);
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
             }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else if (id[i] === "3") {
+            $("#question").text(questions.addition.a300.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.addition.a300.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else {
+            console.log("Something Got Fucked");
+        }
+    } else if (id[0] === "s") {
+        var i = id.length - 1;
+        var valuesIncluded = [];
+        if (id[i] === "1") {
+            $("#question").text(questions.subtraction.s100.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.subtraction.s100.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else if (id[i] === "2") {
+            $("#question").text(questions.subtraction.s200.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.subtraction.s200.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else if (id[i] === "3") {
+            $("#question").text(questions.subtraction.s300.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.subtraction.s300.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else {
+            console.log("Something Got Fucked");
+        }
+    } else if (id[0] === "m") {
+        var i = id.length - 1;
+        var valuesIncluded = [];
+        if (id[i] === "1") {
+            $("#question").text(questions.multiplication.m100.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.multiplication.m100.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else if (id[i] === "2") {
+            $("#question").text(questions.multiplication.m200.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.multiplication.m200.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else if (id[i] === "3") {
+            $("#question").text(questions.multiplication.m300.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.multiplication.m300.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else {
+            console.log("Something Got Fucked");
+        }
+    } else if (id[0] === "d") {
+        var i = id.length - 1;
+        var valuesIncluded = [];
+        if (id[i] === "1") {
+            $("#question").text(questions.division.d100.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.division.d100.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else if (id[i] === "2") {
+            $("#question").text(questions.division.d200.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.division.d200.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else if (id[i] === "3") {
+            $("#question").text(questions.division.d300.question);
+            var answers = ["1", "2", "3", "4"];
+
+            for (var j = 0; j < answers.length; j++) {
+                answers[0] = parseInt(questions.division.d300.answer);
+
+                valuesIncluded.push(createRandomAnswers(id, answers, valuesIncluded, i, j));
+            }
+
+            console.log(answers.join(", "));
+            displayRandomAnswers(answers);
+
+            answerClicks(answers);
+        } else {
+            console.log("Something Got Fucked");
         }
     }
-}
-
-function determineCorrectAnswer() {
-    console.log("fuck you");
-}
-
-function optionPicked() {
-    $("#option1").on("click", function () {
-        determineCorrectAnswer();
-    });
-    $("#option2").on("click", function () {
-        determineCorrectAnswer();
-    });
-    $("#option3").on("click", function () {
-        determineCorrectAnswer();
-    });
-    $("#option4").on("click", function () {
-        determineCorrectAnswer();
-    });
 }
